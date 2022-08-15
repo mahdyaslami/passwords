7121c3042ce20cd500d1e78353a20708bc0db739
mahdi.aslami <mahdi.aslami@partsoftware.com>
2022-08-15 11:39:02 +0430

    Add BaseInput and BaseLabel

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="max-w-xl mx-auto pt-20">
     <div class="mb-4">
-      <label
+      <base-label
         for="search"
-        class="block text-sm font-medium text-gray-700"
-      >Search</label>
+      >
+        Search
+      </base-label>
       <div class="mt-1">
-        <input
+        <base-input
           id="search"
           type="text"
-          class="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm
-            border-gray-300 rounded-md"
           placeholder="blograph.ir valasr"
-        >
+        />
       </div>
     </div>
 
@@ -29,6 +28,8 @@
 
 <script setup>
 import { useDatabaseStore } from '@/stores/database'
+import BaseInput from '@/components/Input'
+import BaseLabel from '@/components/Label'
 import PairCard from './components/PairCard'
 
 const database = useDatabaseStore()
