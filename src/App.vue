9dfef79ee33e72b513b678e07bb1b44e853d5b91
mahdi.aslami <mahdi.aslami@partsoftware.com>
2022-08-15 10:40:56 +0430

    Update style

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <pair-form />
+  <div class="max-w-xl mx-auto pt-20">
+    <div>
+      <label
+        for="search"
+        class="block text-sm font-medium text-gray-700"
+      >Search</label>
+      <div class="mt-1">
+        <input
+          id="search"
+          type="text"
+          class="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm
+            border-gray-300 rounded-md"
+          placeholder="blograph.ir valasr"
+        >
+      </div>
+    </div>
 
     <div
       v-for="row in database.rows"
@@ -16,7 +30,6 @@
 </template>
 
 <script setup>
-import PairForm from '@/components/PairForm'
 import { useDatabaseStore } from '@/stores/database'
 
 const database = useDatabaseStore()
