ee1822ec6612b3d50a36e0af2181aa992e4365ef
mahdi.aslami <mahdi.aslami@partsoftware.com>
2022-08-16 11:30:10 +0430

    Add mb-2 to CredentialCard

--- src/components/CredentialCard.vue
+++ src/components/CredentialCard.vue
@@ -2,7 +2,7 @@
   <div
     class="relative rounded-lg border border-gray-300 bg-white px-4 py-3
             shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2
-            focus-within:ring-offset-2 focus-within:ring-indigo-500"
+            focus-within:ring-offset-2 focus-within:ring-indigo-500 mb-2"
   >
     <div class="flex-1 min-w-0">
       <a
