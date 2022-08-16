b02c9d5af14e549e928eaafae5a3f160f661334c
mahdi.aslami <mahdi.aslami@partsoftware.com>
2022-08-16 11:29:08 +0430

    Update base input

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,6 +1,24 @@
 <template>
   <input
+    v-model="value"
     class="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm
             border-gray-300 rounded-md"
   >
 </template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps(['modelValue'])
+
+const emit = defineEmits(['update:modelValue'])
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(val) {
+    emit('update:modelValue', val)
+  },
+})
+</script>
