9f4d178666ca9d52cda8586520897abcf9bd6076
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2023-08-31 07:30:07 +0330

    Add Auth page

---
+++ src/pages/Auth.vue
@@ -0,0 +1,16 @@
+<script setup>
+import IconGoogle from '@/components/icons/IconGoogle'
+
+</script>
+
+<template>
+  <div class="h-screen w-full flex justify-center items-center">
+    <button
+      class="border rounded-full bg-gray-100 transition-all
+        duration-700 hover:shadow-lg"
+    >
+      <IconGoogle class="inline" />
+      <span class="ml-2 mr-4">Authenticate by Google</span>
+    </button>
+  </div>
+</template>
